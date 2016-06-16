0be7f16c83ed47b837e15023850bd858a7b5c65a
jet <jetlyu521@yahoo.com>
2016-06-16 14:32:21 +0800

    主页基本完成

--- controllers/index.js
+++ controllers/index.js
@@ -44,7 +44,7 @@ exports.images = function (req, data, render) {
  */
 exports.showImages = function (req, data, render) {
 	var querySql = 'select i.* from yatu_images i where i.id=?';
-	console.log(req.params.id);
+	// console.log(req.params.id);
 	db.query(querySql, [req.params.id], function (err, results) {
 		if (err) {
 			console.error("Unexpected error");
@@ -55,3 +55,34 @@ exports.showImages = function (req, data, render) {
 	});
 }
 
+/**
+ * 首页“我们的责任文字”
+ */
+exports.dutiesText = function (req, data, render) {
+	var querySql = 'select t.content from yatu_text t where t.code=?';
+	//此处ID实际上传入的是code
+	db.query(querySql, [req.params.id], function (err, results) {
+		if (err) {
+			console.error("Unexpected error");
+			return;
+		}
+
+		return render(true, results);
+	});
+}
+
+
+exports.videoLink = function (req, data, render) {
+	var querySql = 'select v.* from yatu_videos v where v.id=?';
+	db.query(querySql, [req.params.id], function (err, results) {
+		for(var i in req.params) {
+			console.log(i + " : " + req.params[i]);
+		}
+		if (err) {
+			console.error("Unexpected error");
+			return;
+		}
+
+		return render(true, results);
+	});
+}
